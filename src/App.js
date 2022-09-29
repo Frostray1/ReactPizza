4c79515d14fb8e5460c95503defca113c22d56f1
Artem <a.bychkoff1@yandex.ru>
2022-09-29 15:49:40 +0300

    React.UseEffect + MockApi

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
+import React, { useState } from 'react';
 import './scss/app.scss';
 import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
 
-import pizzas from './assets/pizzas.json'
 
 
 function App() {
+
+const [items, setItems] = React.useState([]);
+
+React.useEffect(()=> {
+  fetch ('https://6332d070573c03ab0b5131fd.mockapi.io/items')
+  .then(res =>{return res.json();})
+  .then((arr)=>{
+  setItems(arr);
+})
+
+}, []);
+
+
+
+
+
+
   return (
     <div className="wrapper">
       <Header/>
       <div className="content">
-          <div className="container">
+          <div className="container"> 
           <div className="content__top">
             <Categories/>
             <Sort/>
@@ -20,8 +37,8 @@ function App() {
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
            {
-            pizzas.map ((obj) => 
-            <PizzaBlock {... obj}
+            items.map ((obj) => 
+            <PizzaBlock key={obj.id} {... obj}
               
               />)
             
